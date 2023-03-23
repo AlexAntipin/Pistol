e0d81e4c6f0db6f4fad6c7d1a9d217360bbeef8d
AlexAntipin <antipin-it@mail.ru>
2023-03-23 21:59:50 +0400

    Initial commit

---
+++ src/Pistol.java
@@ -0,0 +1,17 @@
+public class Pistol {
+    private Integer count_patron;
+
+    public Pistol(){
+        this.count_patron = 5;
+    }
+
+    public Pistol(Integer count_patron){
+        this.count_patron = count_patron;
+    }
+
+    public void
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
