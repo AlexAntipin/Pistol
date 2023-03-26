ae84169c6638a376e06b8bd30083b2b22ff3b6ce
AlexAntipin <antipin-it@mail.ru>
2023-03-26 13:21:29 +0400

    Добавил max_count_patron

--- src/Pistol.java
+++ src/Pistol.java
@@ -1,15 +1,34 @@
 public class Pistol {
     private Integer count_patron;
+    private Integer max_count_patron;
 
     public Pistol(){
         this.count_patron = 5;
     }
 
     public Pistol(Integer count_patron){
+        this.max_
         this.count_patron = count_patron;
     }
 
-    public void
+    public void Shoot(){
+        if (this.count_patron > 0){
+            this.count_patron --;
+            System.out.println("Бах!");
+        }
+        else {
+            System.out.println("Клац!");
+        }
+    }
+
+
+    public void Checkpatron(){
+        System.out.println(this.count_patron);
+    }
+
+
+
+
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
