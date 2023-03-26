a540aec18eaa7a74907e5bbf76f65acbaf509ffb
AlexAntipin <antipin-it@mail.ru>
2023-03-26 14:30:27 +0400

    Финальные изменения

--- src/Pistol.java
+++ src/Pistol.java
@@ -1,13 +1,14 @@
 public class Pistol {
     private Integer count_patron;
-    private Integer max_count_patron;
+    final Integer max_count_patron;
 
-    public Pistol(){
+    public Pistol(Integer max_count_patron){
         this.count_patron = 5;
+        this.max_count_patron = max_count_patron;
     }
 
-    public Pistol(Integer count_patron){
-        this.max_
+    public Pistol(Integer count_patron, Integer max_count_patron){
+        this.max_count_patron = max_count_patron;
         this.count_patron = count_patron;
     }
 
@@ -26,11 +27,51 @@ public class Pistol {
         System.out.println(this.count_patron);
     }
 
+    public Integer Unload(){
+        Integer help = count_patron;
+        this.count_patron = 0;
+        return help;
+    }
 
+    public Integer Reload(Integer count_patron_reload){
+        if (count_patron_reload < 0) {
+            System.out.println("Введено неправильное количество патронов");
+            return null;
+        }
+        else if (this.count_patron + count_patron_reload > this.max_count_patron){
+            Integer return_patron = (this.count_patron + count_patron_reload) - this.max_count_patron;
+            this.count_patron = this.max_count_patron;
+            return return_patron;
+        }
+        else{
+            this.count_patron += count_patron_reload;
+            return 0;
+        }
+
+    }
+
+    public boolean isLoaded(){
+        if (this.count_patron > 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Pistol test = new Pistol(3,7);
+        for (int i = 0; i < 5; i++){
+            test.Shoot();
+        }
+        test.Reload(8);
+        test.Shoot();
+        test.Shoot();
+
+        test.Unload();
+        test.Shoot();
+
     }
 }
